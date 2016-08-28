8a734c63479e40664316e118c32ab245f44212eb
Martin Perry <martin.perry@careerbuilder.com>
2016-08-27 23:55:19 -0400

    Adds events for the text boxes

--- Control Fun/ViewController.m
+++ Control Fun/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *numberField;
 
 @end
 
@@ -22,6 +24,10 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)textFieldDoneEditing:(id)sender {
+    [sender resignFirstResponder];
 }
 
 @end
